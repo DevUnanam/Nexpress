4f623ec934f35d282359d03a36692a1b33f877c7
Smashlord564 <smashking753@gmail.com>
2025-11-08 13:09:24 +0100

    corrected sendgrid credential

--- fedex_clone/settings.py
+++ fedex_clone/settings.py
@@ -166,7 +166,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 600  # Extended timeout for WSL
-EMAIL_HOST_USER = env("apikey")
+EMAIL_HOST_USER = "apikey"
 EMAIL_HOST_PASSWORD = env("SENDGRID_API_KEY")
 DEFAULT_FROM_EMAIL = env("riaahh20@gmail.com")
 
